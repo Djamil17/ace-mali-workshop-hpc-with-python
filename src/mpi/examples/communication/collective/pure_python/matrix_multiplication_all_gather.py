61c5bc97b50cc8cf5cf31e26ed3b4a21dda4d76d
Djamil17 <dl2774@columbia.edu>
2024-05-21 11:45:55 -0400

    feat(mpi): implements parallel matrix multiplication

--- src/mpi/examples/communication/collective/pure_python/matrix_multiplication_all_gather.py
+++ src/mpi/examples/communication/collective/pure_python/matrix_multiplication_all_gather.py
@@ -1,11 +1,33 @@
 """
-Perform matrix-vector multiplication
+Perform matrix-matrix multiplication then have matrix distributed
+to each process.
+
+Date: 05/21/2024
+Author: Djamil Lakhdar-Hamina
 
 """
 from mpi4py import MPI
 
 
 class Matrix:
+    """
+    A class representing a matrix including various attributes
+    and methods.
+
+    Attributes:
+        dimensions (tuple(int,int)): dimension of matrix
+
+    Methods:
+        setitem: set the item using a tuple
+        getitem: get the item using a tuple or slice
+        str: produce a string rep for print
+        T: produce transpose of matrix
+        equal: dimensional and elementwise
+        add: add two matrices
+        multiply: multiply two matrices
+
+    """
+
     def __init__(self, A=[[]]) -> None:
         self.mat = A
         self.dimensions = (len(self.mat), len(self.mat[0]))
@@ -32,22 +54,22 @@ class Matrix:
                 # Column access with row slicing
                 rows = self._process_slice(index[0], len(self.mat))
                 col = index[1]
-                sliced_matrix = [[self.mat[i][col]] for i in rows]
-                return Matrix(sliced_matrix)
+                sliced_matriB = [[self.mat[i][col]] for i in rows]
+                return Matrix(sliced_matriB)
             elif all(isinstance(k, slice) for k in index):
                 rows = self._process_slice(index[0], len(self.mat))
                 cols = self._process_slice(index[1], len(self.mat[0]))
-                sliced_matrix = [
+                sliced_matriB = [
                     [self.mat[i][j] for j in range(len(self.mat[0])) if j in cols]
                     for i in rows
                 ]
-                return Matrix(sliced_matrix)
+                return Matrix(sliced_matriB)
         else:
             raise TypeError("Invalid key type")
 
-    def _process_slice(self, slic, max_size) -> range:
+    def _process_slice(self, slic, maB_size) -> range:
         start = slic.start if slic.start is not None else 0
-        stop = slic.stop if slic.stop is not None else max_size
+        stop = slic.stop if slic.stop is not None else maB_size
         step = slic.step if slic.step is not None else 1
         return range(start, stop, step)
 
@@ -67,6 +89,15 @@ class Matrix:
                 v.append(self[i, j])
             A_T.append(v)
         return Matrix(A_T)
+
+    def __eq__(self, B: "Matrix") -> "Matrix":
+        assert B.dimensions == self.dimensions
+        m, n = self.dimensions
+        for i in range(m):
+            for j in range(n):
+                if self[i, j] != B[i, j]:
+                    return False
+        return True
 
     def __add__(self, B: "Matrix") -> "Matrix":
         C = []
@@ -100,28 +131,34 @@ def main():
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    A = Matrix([[1, 2], [4, 5]])
-    x = Matrix([[1, 2], [4, 5]])
+    A = Matrix([[1, 2, 3], [3, 4, 5]])
+    B = Matrix([[1, 2], [3, 4], [4, 5]])
     m, _ = A.dimensions
-    _, n = x.dimensions
-    y = Matrix([[0 for j in range(n)] for i in range(m)])
+    _, n = B.dimensions
+    y = Matrix([[0 for _ in range(n)] for _ in range(m)])
 
     if rank == 0:
         try:
-            m, _ = A.dimensions
-            _, n = x.dimensions
-            if m == size:
-                pass
+            m, r = A.dimensions
+            s, n = B.dimensions
+            if m != size and n != size:
+                raise ValueError("# rows does equal # process")
+            if r != s:
+                raise ValueError("dimensions of matrices invalid")
         except MPI.Exception as e:
             print(f"{e}: the number of rows does not equal number of processors")
 
-    print(A[:, rank])
-    local_x = A[rank, :] * x
-    print(local_x)
-    y = comm.allgather(local_x)
+    n = comm.bcast(n, root=0)
+    local_row = []
+    for i in range(n):
+        local_row.append((A[rank, :] * B[:, i])[0, 0])
 
-    if rank == 0:
-        print(y)
+    y = comm.allgather(local_row)
+
+    assert A * B == Matrix(y)
+
+    return y
 
 
-main()
+if __name__ == "__main__":
+    main()
