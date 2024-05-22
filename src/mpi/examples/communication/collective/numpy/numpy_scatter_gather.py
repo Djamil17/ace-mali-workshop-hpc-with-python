1eb2915964c98381e95d65837465a0359faabbec
Djamil17 <dl2774@columbia.edu>
2024-05-22 11:09:07 -0400

    feat(mpi): implement numpy parallel vector addition

--- src/mpi/examples/communication/collective/numpy/numpy_scatter_gather.py
+++ src/mpi/examples/communication/collective/numpy/numpy_scatter_gather.py
@@ -1,3 +1,10 @@
+"""
+Use numpy array and mpi4py to add two vectors together.
+
+Date: 05/22/2024
+Author: Djamil Lakhdar-Hamin
+"""
+
 from itertools import islice
 
 import numpy as np
@@ -20,51 +27,54 @@ def partition(iterable: iter, n: int) -> list[list]:
 
 def main():
     ROOT = 0
-    VECTOR_LENGTH = 10
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    chunk = VECTOR_LENGTH // (size)
-
-    np.random.seed(19)
-
-    sendbuf = None
-    recvbuf = np.empty(chunk * 2)
-
     if rank == 0:
+        print("input vector length:")
+        n = int(input())
+        chunk = n // (size)
+        info = np.array([n, chunk], dtype=int)
+        print(f"number elements per process: {chunk}")
         try:
-            if VECTOR_LENGTH % size != 0:
+            if n % size != 0:
                 raise ValueError(
-                    "The values are not evenly partitioned between n-1 worker\
+                    "The values are not evenly partitioned between n workers\
 processors"
                 )
         except ValueError as e:
             print(e)
             comm.Abort()
-        # define 2 vectors
-        x, y = np.random.rand(VECTOR_LENGTH), np.random.rand(VECTOR_LENGTH)
-        partitions = np.array(partition(x, chunk)), np.array(partition(y, chunk))
+        # define 2 random vectors
+        x, y = np.random.rand(n), np.random.rand(n)
+        partitions = np.array(partition(x, chunk), dtype=float), np.array(
+            partition(y, chunk), dtype=float
+        )
         # split each vector into p chunks of n size
         sendbuf = np.array(
             [np.concatenate((part_x, part_y)) for (part_x, part_y) in zip(*partitions)]
         )
-        print(f"sendbuf is : \n {sendbuf}\n")
+        print(f"split it up and concatenated, sendbuf is : \n {sendbuf}\n")
+    else:
+        info = np.zeros(2, dtype=int)
+        sendbuf = None
 
-    comm.Scatter(sendbuf, recvbuf, root=ROOT)
+    comm.Bcast([info, MPI.INT], root=0)
+    n, chunk = info
+    recvbuf = np.zeros(chunk * 2, dtype=float)
+
+    comm.Scatter([sendbuf, MPI.FLOAT], [recvbuf, MPI.FLOAT], root=ROOT)
     midpoint = len(recvbuf) // 2
     local_sum = recvbuf[:midpoint] + recvbuf[midpoint:]
 
-    finalrecvbuf = None
-    if rank == 0:
-        finalrecvbuf = np.empty((size, chunk))
-
-    comm.Gather(local_sum, finalrecvbuf, root=ROOT)
-
+    finalrecvbuf = np.zeros((size, chunk))
+    comm.Gather([local_sum, MPI.FLOAT], [finalrecvbuf, MPI.FLOAT], root=ROOT)
     if rank == 0:
         print(f" {x} + {y} = {finalrecvbuf.flatten()}")
         assert np.all(x + y == finalrecvbuf.flatten())
 
 
-main()
+if __name__ == "__main__":
+    main()
