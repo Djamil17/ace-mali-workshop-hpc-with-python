577009b7f16fd4e2b5f50dc4e21dae417561e519
Djamil17 <dl2774@columbia.edu>
2024-05-23 13:10:07 -0400

    feat(mpi): exhibit async nonblocking communication with imethods

--- src/mpi/examples/communication/non-blocking/imethods.py
+++ src/mpi/examples/communication/non-blocking/imethods.py
@@ -1,5 +1,14 @@
 """
+This program demonstrates basic MPI (Message Passing Interface) asynchronous
+communication usingthe mpi4py library. The example involves two processes
+(ranks 0 and 1) exchanging messages.
 
+Usage:
+    Run the script with an MPI execution command such as
+   `mpiexec -n 2 python script_name.py`.
+
+Date: 05/23/2024
+Author: Djamil Lakhdar-Hamina
 
 """
 
@@ -10,8 +19,10 @@ from mpi4py import MPI
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
+    size = comm.Get_size()
     status = MPI.Status()
 
+    assert size == 2
     N = 10
     # non numpy-example
     if rank == 0:
@@ -19,23 +30,23 @@ def main():
         request = comm.Isend(data, dest=1)
         print(2 + 3)
     else:
-        buf = bytearray(54)
+        buf = bytearray(46)
         request = comm.Irecv(buf, source=0)
-        print(2 + 3)
+        print(6 + 4)
         request.wait(status=status)
         print(f"{buf.decode()} From process {rank}")
 
     # numpy-example
     if rank == 0:
         data = np.random.rand(N)
-        request = comm.Isend(data, dest=1)
+        request = comm.Isend([data, MPI.FLOAT], dest=1)
         print(2 + 3)
     else:
         buf = np.empty(N)
-        request = comm.Irecv(buf, source=0)
-        print(2 + 3)
+        request = comm.Irecv([buf, MPI.FLOAT], source=0)
+        print(3 + 3)
         request.wait(status=status)
-        print(f"from process {rank}: {buf}")
+        print(f"f process {rank}: {buf}")
 
 
 if __name__ == "__main__":
