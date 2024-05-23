18a158e417099ec6aaa4096b89f145d22ccf6fed
Djamil17 <dl2774@columbia.edu>
2024-05-23 12:39:59 -0400

    feat(mpi): illustrates basic get put model

--- src/mpi/examples/communication/non-blocking/get_put.py
+++ src/mpi/examples/communication/non-blocking/get_put.py
@@ -1,3 +1,14 @@
+"""
+Show use of get put model. Program instantiates a window of
+shared memory which only exists in process 0. Process 0
+fills a buffer, or puts data, and then the other processes
+get that data from shared memory.
+
+Date: 05/22/2024
+Author: Djamil Lakhdar-Hamina
+
+"""
+
 import numpy as np
 from mpi4py import MPI
 from mpi4py.util import dtlib
@@ -18,19 +29,17 @@ def main():
 
     if rank == 0:
         buf.fill(42)
-        win.Lock(rank=0)
+        win.Lock(rank=0, lock_type=MPI.LOCK_EXCLUSIVE, assertion=MPI.MODE_NOCHECK)
         win.Put(buf, target_rank=0)
         win.Unlock(rank=0)
         comm.Barrier()
     else:
         comm.Barrier()
-        win.Lock(rank=0)
+        win.Lock(rank=0, lock_type=MPI.LOCK_SHARED, assertion=MPI.MODE_NOCHECK)
         win.Get(buf, target_rank=0)
         win.Unlock(rank=0)
         assert np.all(buf == 42)
 
-    if rank == 0:
-        print("We filled all the worker nodes with arrays of 42! ")
 
-
-main()
+if __name__ == "__main__":
+    main()
